182a32f42a4ed3660249d69e63e3373a8294841b
jadedrb <jadecdrb@gmail.com>
2023-12-07 13:13:16 -0500

    added crypto-prices app

---
+++ MOD2/Week-3/Day-3-Thur/crypto-prices/src/pages/Price.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+
+export default function Price() {
+
+    let apiKey = import.meta.env.VITE_API_URL
+
+    let { symbol } = useParams()
+
+    const url = `http://rest.coinapi.io/v1/exchangerate/${symbol}/USD?apikey=${apiKey}`;
+
+    const [coin, setCoin] = useState({})
+
+    async function getCoin() {
+        try {
+            console.log('fetching...')
+            const response = await fetch(url)
+            const data = await response.json()
+            console.log('setting state...')
+            setCoin(data)
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getCoin()
+    }, [])
+
+    function loaded() {
+        return (
+        <div>
+            <h1>{coin.asset_id_base} / {coin.asset_id_quote}</h1>
+            <h2>{coin.rate}</h2>
+        </div>
+        )
+    }
+
+    function loading() {
+        return (
+            <div>Loading...</div>
+        )
+    }
+
+    return coin.rate ? loaded() : loading()
+}
