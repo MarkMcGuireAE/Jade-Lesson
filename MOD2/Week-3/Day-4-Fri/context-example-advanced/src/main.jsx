366cc3a7db46a6aa727eecbbcd048140f616e2e0
jadedrb <jadecdrb@gmail.com>
2023-12-08 12:00:14 -0500

    added a more advanced context example and the css module example from the morning

--- MOD2/Week-3/Day-4-Fri/context-example-advanced/src/main.jsx
+++ MOD2/Week-3/Day-4-Fri/context-example-advanced/src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { FamilyProvider } from './familyContext.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <FamilyProvider>
+      <App />
+    </FamilyProvider>
   </React.StrictMode>,
 )
