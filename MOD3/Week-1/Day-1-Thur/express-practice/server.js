751c54c20d186284c95b806e202ae412ef878a6d
jadedrb <jadecdrb@gmail.com>
2024-01-11 12:05:18 -0500

    url params example

--- MOD3/Week-1/Day-1-Thur/express-practice/server.js
+++ MOD3/Week-1/Day-1-Thur/express-practice/server.js
@@ -1,8 +1,12 @@
+let colors = ['red', 'blue', 'green', 'orange']
+
 // Require modules
 const express = require('express');
 
 // Create the Express app
 const app = express();
+
+const PORT = 3000
 
 // Configure the app (app.set)
 
@@ -12,10 +16,32 @@ const app = express();
 
 // Mount routes
 app.get('/', function(req, res) {
-  	res.send('<h1>Hello World!</h1>');
+  	res.send('<h1>Hello Expresss!</h1>');
 });
 
+app.get('/home', function(req, res) {
+    res.send('<h1>Home Page</h1>')
+})
+
+app.get('/:test', function(req, res) {
+    res.send('<h1>Test</h1>')
+})
+
+app.get('/:test2', function(req, res) {
+    res.send('<h1>Test</h1>')
+})
+
+
+
+app.get('/colors/:indexOfColor', (req, res) => {
+    if (colors[req.params.indexOfColor]) {
+        res.send(colors[req.params.indexOfColor]);
+  } else {
+    res.send('cannot find anything at this index: ' + req.params.indexOfColor);
+  }
+})
+
 // Tell the app to listen on port 3000
-app.listen(3000, function() {
- console.log('Listening on port 3000');
+app.listen(PORT, function() {
+ console.log(`Listening on port ${PORT}`);
 });
