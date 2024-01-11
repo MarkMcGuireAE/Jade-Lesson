1b1b5851b83a5c86468bf5c0e85b4af0f387fc0b
jadedrb <jadecdrb@gmail.com>
2024-01-11 10:58:16 -0500

    added basic structure

--- MOD3/Week-1/Day-1-Thur/express-practice/server.js
+++ MOD3/Week-1/Day-1-Thur/express-practice/server.js
@@ -1,11 +1,21 @@
-const express = require('express')
+// Require modules
+const express = require('express');
 
-const app = express()
+// Create the Express app
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
-})
+// Configure the app (app.set)
 
-app.listen(5000, () => {
-    console.log('Listening on port 5000')
-})
+
+// Mount middleware (app.use)
+
+
+// Mount routes
+app.get('/', function(req, res) {
+  	res.send('<h1>Hello World!</h1>');
+});
+
+// Tell the app to listen on port 3000
+app.listen(3000, function() {
+ console.log('Listening on port 3000');
+});
