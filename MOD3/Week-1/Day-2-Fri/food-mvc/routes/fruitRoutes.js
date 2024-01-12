27f0b2f018ee5df27a4a72125b6b79eb1b2d63d9
jadedrb <jadecdrb@gmail.com>
2024-01-12 14:04:22 -0500

    added a router

---
+++ MOD3/Week-1/Day-2-Fri/food-mvc/routes/fruitRoutes.js
@@ -0,0 +1,57 @@
+const express = require('express')
+
+const router = express.Router()
+
+// "index" route
+router.get('/fruits', (req, res) => {
+    res.render('Index', { fruits: fruits })
+})
+
+// "new" route
+router.get('/fruits/new', (req, res) => {
+    res.render('New')
+})
+
+// "show" route
+router.get('/fruits/:index', (req, res) => {
+    res.render('Show', { fruit: fruits[req.params.index], index: req.params.index })
+    // res.send(fruits[req.params.index])
+})
+
+// "create" route
+router.post('/fruits', (req, res) => {
+    console.log(req.body)
+    if (req.body.readyToEat === 'on') {
+        req.body.readyToEat = true
+    } else {
+        req.body.readyToEat = false
+    }
+    fruits.push(req.body)
+    res.redirect('/fruits')
+})
+
+// "edit" route
+router.get('/fruits/:index/edit', (req, res) => {
+    res.render('Edit', { fruit: fruits[req.params.index], index: req.params.index })
+})
+
+// "destroy" route
+router.delete('/fruits/:index', (req, res) => {
+    fruits.splice(req.params.index, 1)
+    console.log('deleting')
+    res.redirect('/fruits')
+})
+
+// "update" route
+router.put('/fruits/:index', (req, res) => {
+    console.log('update')
+    if (req.body.readyToEat === 'on') {
+        req.body.readyToEat = true
+    } else {
+        req.body.readyToEat = false
+    }
+    fruits[req.params.index] = req.body
+    res.redirect(`/fruits/${req.params.index}`)
+})
+
+module.exports = router
--- MOD3/Week-1/Day-2-Fri/food-mvc/server.js
+++ MOD3/Week-1/Day-2-Fri/food-mvc/server.js
@@ -23,59 +23,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-// "index" route
-app.get('/fruits', (req, res) => {
-    res.render('Index', { fruits: fruits })
-})
-
-// "new" route
-app.get('/fruits/new', (req, res) => {
-    res.render('New')
-})
-
-// "show" route
-app.get('/fruits/:index', (req, res) => {
-    res.render('Show', { fruit: fruits[req.params.index], index: req.params.index })
-    // res.send(fruits[req.params.index])
-})
-
-// "create" route
-app.post('/fruits', (req, res) => {
-    console.log(req.body)
-    if (req.body.readyToEat === 'on') {
-        req.body.readyToEat = true
-    } else {
-        req.body.readyToEat = false
-    }
-    fruits.push(req.body)
-    res.redirect('/fruits')
-})
-
-// "edit" route
-app.get('/fruits/:index/edit', (req, res) => {
-    res.render('Edit', { fruit: fruits[req.params.index], index: req.params.index })
-})
-
-// "destroy" route
-app.delete('/fruits/:index', (req, res) => {
-    fruits.splice(req.params.index, 1)
-    console.log('deleting')
-    res.redirect('/fruits')
-})
-
-// "update" route
-app.put('/fruits/:index', (req, res) => {
-    console.log('update')
-    if (req.body.readyToEat === 'on') {
-        req.body.readyToEat = true
-    } else {
-        req.body.readyToEat = false
-    }
-    fruits[req.params.index] = req.body
-    res.redirect(`/fruits/${req.params.index}`)
-})
-
-
 app.listen(PORT, () => {
     console.log('Listening on port: ' + PORT)
 })
