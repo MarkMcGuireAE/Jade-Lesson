6d240be8ec111ec40ad1a62b6974e62106fab2eb
jadedrb <jadecdrb@gmail.com>
2024-01-12 14:07:58 -0500

    added routes to server as middleware

--- MOD3/Week-1/Day-2-Fri/food-mvc/routes/fruitRoutes.js
+++ MOD3/Week-1/Day-2-Fri/food-mvc/routes/fruitRoutes.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const router = express.Router()
 
+const fruits = require('./models/fruits')
+
 // "index" route
 router.get('/fruits', (req, res) => {
     res.render('Index', { fruits: fruits })
--- MOD3/Week-1/Day-2-Fri/food-mvc/server.js
+++ MOD3/Week-1/Day-2-Fri/food-mvc/server.js
@@ -3,16 +3,20 @@ const methodOverride = require('method-override')
 
 const app = express()
 
-const fruits = require('./models/fruits')
+// const fruits = require('./models/fruits')
 
 const PORT = 5000
 
 // load our engine
 const jsxEngine = require('jsx-view-engine')
 
+// bring in the router
+const fruitRoutes = require('./routes/fruitRoutes')
+
 // format our POST request data 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
+app.use('/fruits', fruitRoutes)
 
 // setup our engine
 app.set('view engine', 'jsx');
