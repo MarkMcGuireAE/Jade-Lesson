d2c5a5dee77bb709a319fc7fea23b13be38cf544
jadedrb <jadecdrb@gmail.com>
2024-01-12 11:04:08 -0500

    added a form to new view

--- MOD3/Week-1/Day-2-Fri/food-mvc/views/New.jsx
+++ MOD3/Week-1/Day-2-Fri/food-mvc/views/New.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function New() {
-    return (
-        <div>New Page</div>
-    )
+  return (
+    <div>
+      <h1>New Page</h1>
+      <form action="/fruits" method="POST">
+        Name: <input type="text" name="name" />
+        <br />
+        Color: <input type="text" name="color" />
+        <br />
+        Is Ready To Eat: <input type="checkbox" name="readyToEat" />
+        <br />
+        <input type="submit" name="" value="Create Fruit" />
+      </form>
+    </div>
+  );
 }
