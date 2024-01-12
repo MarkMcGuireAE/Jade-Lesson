23810db00b07de492d51ec51798a45a31da2e408
jadedrb <jadecdrb@gmail.com>
2024-01-12 09:31:26 -0500

    added more routes

--- MOD3/Week-1/Day-2-Fri/food-mvc/server.js
+++ MOD3/Week-1/Day-2-Fri/food-mvc/server.js
@@ -2,10 +2,23 @@ const express = require('express')
 
 const app = express()
 
+const fruits = require('./models/fruits')
+
 const PORT = 5000
 
+// "root" route
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+// "index" route
+app.get('/fruits', (req, res) => {
+    res.send(fruits)
+})
+
+// "show" route
+app.get('/fruits/:index', (req, res) => {
+    res.send(fruits[req.params.index])
 })
 
 app.listen(PORT, () => {
