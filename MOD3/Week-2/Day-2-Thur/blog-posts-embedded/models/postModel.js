f78e531fd82056aa7caa7ef2c06408184f885c43
jadedrb <jadecdrb@gmail.com>
2024-01-18 09:36:46 -0500

    updated schema with comment sfield

--- MOD3/Week-2/Day-2-Thur/blog-posts-embedded/models/postModel.js
+++ MOD3/Week-2/Day-2-Thur/blog-posts-embedded/models/postModel.js
@@ -8,6 +8,10 @@ const postSchema = new Schema({
    body: { type: String },
    user: { type: String, default: 'Bob' },
    // comments field here
+   comments: [{ 
+      text: { type: String },
+      user: { type: String, default: 'Bob' }
+   }]
 }, { timestamps: true })
 
 const Post = mongoose.model('posts', postSchema)
