0c3d93845140dc4799bf8bad5fb195de97dfc42f
jadedrb <jadecdrb@gmail.com>
2024-01-22 10:17:58 -0500

    updated register route

--- MOD3/Week-3/Day-1-Mon/login-auth-starter/backend/controllers/authController.js
+++ MOD3/Week-3/Day-1-Mon/login-auth-starter/backend/controllers/authController.js
@@ -1,27 +1,46 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 async function register(req, res) {
 
-    // 1. Check if the user exists 
+    try {
+        // 1. Check if the user exists 
 
-    const foundUser = await User.findOne({ username: req.body.username })
+        const foundUser = await User.findOne({ username: req.body.username })
 
-    if (foundUser) {
-        return res.status(400).json({ error: 'User already exists' })
+        if (foundUser) {
+            return res.status(400).json({ error: 'User already exists' })
+        }
+
+        // 2. If they don't (new user... good!) encrypt the password
+
+        const encryptedPassword = await bcrypt.hash(req.body.password, 10)
+
+        console.log(encryptedPassword)
+
+        // 3. Add new user to the database (with the encrypted password)
+
+        console.log({ ...req.body, password: encryptedPassword })
+
+        const newUser = await User.create({ ...req.body, password: encryptedPassword })
+
+        console.log(newUser)
+
+        // 4. Generate a JWT token (the keys... permission slip... wrist band) and returning it to the user 
+            
+        const payload = { id: newUser._id, username: newUser.username }
+        const token = jwt.sign(payload, 'super secret string', { expiresIn: 30 })
+
+        console.log(token)
+
+        res.status(200).json({ token })
+
+    } catch(err) {
+        console.log(err.message)
+        res.status(400).json({ error: err.message })
     }
 
-    // 2. If they don't (new user... good!) encrypt the password
-
-    const encryptedPassword = await bcrypt.hash(req.body.password, 10)
-
-    console.log(req.body.password)
-    console.log(encryptedPassword)
-
-    // 3. Add new user to the database (with the encrypted password)
-
-    // 4. Generate a JWT token (the keys... permission slip... wrist band) and returning it to the user 
-        
 }
 
 async function login(req, res) {
